8ea4238a3f24665bf37d34f2561b3042954c5b9e
Joel Turske <joelturske@gmail.com>
2024-11-01 08:46:41 -0400

    Extract digit exercise.

---
+++ basic_ops/extract_digit.py
@@ -0,0 +1,12 @@
+one_place = 4936 % 10
+print("1's place:", one_place)
+
+ten_place = (4936 % 100) // 10
+print("10's place:", ten_place)
+
+hundreds = (4936 % 1000) // 100
+print("100's place:", hundreds)
+
+thousands = (4936 % 10000) // 1000
+print("1000's place:", thousands)
+
