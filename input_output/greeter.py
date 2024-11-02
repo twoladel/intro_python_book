818c0700303fe068d539ed673e8dca0171781330
Joel Turske <joelturske@gmail.com>
2024-11-02 16:22:00 -0400

    updated for exercise 2.

--- input_output/greeter.py
+++ input_output/greeter.py
@@ -1,2 +1,3 @@
-name = input('What is your name? ')
-print(f'Hello, {name}!')
+first_name = input('What is your first name? ')
+last_name = input('What is your last name? ')
+print(f'Hello, {first_name} {last_name}!')
