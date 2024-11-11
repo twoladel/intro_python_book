7ceb962075fd4e91950f03c8beead2f4927948b2
Joel Turske <joelturske@gmail.com>
2024-11-11 17:37:37 -0500

    caps_long exercise

---
+++ flow_control/caps_plus10.py
@@ -0,0 +1,11 @@
+def caps_plus10(string):
+    if len(string) > 10:
+        return(string.upper())
+    else:
+        return(string)
+
+print(caps_plus10('here is johnny'))
+print(caps_plus10('joel'))
+print(caps_plus10('welcome to my humble abode.'))
+print(caps_plus10('box box'))
+
