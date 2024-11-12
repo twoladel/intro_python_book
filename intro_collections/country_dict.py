ba6add06faf41724a2804c1d9423c133e64642ef
Joel Turske <joelturske@gmail.com>
2024-11-11 21:27:32 -0500

    country_dict.py

---
+++ intro_collections/country_dict.py
@@ -0,0 +1,7 @@
+country_dict = {'Alice': 'USA', 'Francois': 'Canada', 'Inti': 'Peru',
+            'Monika': 'Germany', 'Sanyu': 'Uganda', 'Yoshitaka': 'Japan'}
+
+
+print(country_dict['Alice'])
+print(country_dict['Inti'])
+print(country_dict['Yoshitaka'])
