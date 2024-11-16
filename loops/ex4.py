bb78a345cd964b029d579193ab37b125e75acb52
Joel Turske <joelturske@gmail.com>
2024-11-16 15:15:33 -0500

    even while loop, odd for loop.

---
+++ loops/ex4.py
@@ -0,0 +1,13 @@
+my_list = [6, 3, 0, 11, 20, 4, 17]
+
+index = 0
+while index < len(my_list):
+    if my_list[index] % 2 == 0:
+        print(my_list[index])
+    index += 1
+
+        
+
+for number in my_list:
+    if number % 2 != 0:
+        print(number)
