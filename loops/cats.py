9677abae672649a8c1aa41568b63cc2287ee7c4e
Joel Turske <joelturske@gmail.com>
2024-11-17 10:25:22 -0500

    more messing around with list comprehension in cats.py

--- loops/cats.py
+++ loops/cats.py
@@ -4,12 +4,20 @@ cats_colors = {
     'Fluffy': 'gray',
     'Ben':    'black',
     'Kat':    'orange',
+    'Larry':  'orange',
+    'Lenny':  'orange',
 }
 
-orange_cats = [name for name in cats_colors
-                if cats_colors[name] == 'orange']
+orange_cats = [name.replace('L', 'K') for name in cats_colors
+               if 'L' in name]
 print(orange_cats)
 
 non_orange_cats = [name for name in cats_colors
-                   if cats_colors[name] != 'orange']
+                   if cats_colors[name] != 'orange'
+                   if cats_colors[name] != 'black']
 print(non_orange_cats)
+
+l_orange_cats = [name for name in cats_colors
+                 if cats_colors[name] == 'orange'
+                 if name[:2] == 'Le']
+print(l_orange_cats)
